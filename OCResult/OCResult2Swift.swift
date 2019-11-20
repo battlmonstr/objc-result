130a551441d56050df16c940d8cffd0391de94d2
battlmonstr <battlmonstr@users.noreply.github.com>
2019-11-20 01:06:57 +0100

    add Xcode docs for the Swift parts

--- OCResult/OCResult2Swift.swift
+++ OCResult/OCResult2Swift.swift
@@ -1,6 +1,13 @@
 import Foundation
 
+/**
+ Converter between `OCResult` and the standard Swift `Result<Success, Failure>` type.
+ */
 extension OCResult {
+    /**
+     Converts an `OCResult` to the Swift's `Result` object.
+     The value type becomes `Any`, but you can use `Result.map()` or `Result.flatMap()` to convert it to a desired type.
+     */
     func toSwift() -> Result<Any, Error> {
         let result = self
         switch result.kind {
@@ -13,6 +20,9 @@ extension OCResult {
         }
     }
 
+    /**
+     Converts a given Swift's `Result` to an `OCResult` object.
+     */
     static func make<Success : AnyObject, Failure : Error>(fromSwiftResult result: Result<Success, Failure>) -> OCResult {
         switch result {
         case .success(let value):
